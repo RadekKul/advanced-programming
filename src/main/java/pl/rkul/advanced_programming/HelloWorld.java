8832d2af53370aab69411b57241f80623b508838
Radek <kulikowskiradek@o2.pl>
2019-02-02 11:00:01 +0100

    {advanced programming start1}

---
+++ src/main/java/pl/rkul/advanced_programming/HelloWorld.java
@@ -0,0 +1,17 @@
+package pl.rkul.advanced_programming;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+
+        Runnable x;
+
+
+        x = ()->{};
+
+
+    }
+}
